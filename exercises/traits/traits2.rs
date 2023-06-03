aa6c4e70931964510c19f3ada465f7e514523e7a
k5924 <k.bhatti5384@yahoo.co.uk>
2023-06-03 18:41:33 +0100

    Subject: Generics and Traits
    Problem:
    Solve exercises on generics and traits
    
    Solution:
    Solved exericses on generics and traits
    
    Note:
    Next commit will involve quiz 3

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -11,10 +11,17 @@
 // you can do this!
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 trait AppendBar {
     fn append_bar(self) -> Self;
+}
+
+impl AppendBar for Vec<String> {
+    // add code here
+    fn append_bar(mut self) -> Self {
+        self.push(String::from("Bar"));
+        self
+    }
 }
 
 // TODO: Implement trait `AppendBar` for a vector of strings.
