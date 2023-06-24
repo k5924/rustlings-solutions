54262760fe1b6f9b76767e9f6ee97c6e9b5c0079
k5924 <k.bhatti5384@yahoo.co.uk>
2023-06-24 17:55:20 +0100

    Subject: Finished Rustlings
    Problem: Solve Conversions Exercises

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,27 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+
+        let parts: Vec<_> = s.split(',').collect();
+        if parts[0].len() == 0 || parts.len() != 2 {
+            return Person::default();
+        }
+
+        let age = parts[1].parse::<usize>();
+        if let Ok(age) = age {
+            return Person {
+                name: parts[0].into(),
+                age: age.into()
+            };
+        }
+
+        return Person::default();
     }
 }
 
